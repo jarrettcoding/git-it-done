84f9a4a87483e7dbb914fe2fe662ddcd099dc224
veritascoding <jarrettcoding@gmail.com>
2021-12-08 21:47:31 -0600

    add error handling to single.js, finish out single.js scripting

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,19 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        getRepoIssues(repoName);
+        repoNameEl.textContent = repoName
+    } else {
+        document.location.replace("./index.html");
+    }
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo +"/issues?direction=asc";
@@ -7,9 +22,14 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
-            alert("There was a problem with your request.");
+            document.location.replace("./index.html");
         }
     });
 };
@@ -51,4 +71,17 @@ var displayIssues = function(issues) {
     }
 };
 
-getRepoIssues("facebook/react");
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+}
+
+getRepoName();
